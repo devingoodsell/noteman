39ab054a24233ba61575fb010da8aae8d49461fd
Devin Goodsell <devingoodsell@gmail.com>
2015-01-30 15:54:29 -0800

    Added additional config vars.

--- config/env/development.js
+++ config/env/development.js
@@ -2,7 +2,7 @@
 "use strict";
 
 module.exports = {
-  db:       "mongodb://localhost/comingsoon",
+  db:       "mongodb://heroku_app33077384:qq4q0652v7hjk6uls6ki3u4r4f@ds053788.mongolab.com:53788/heroku_app33077384", //"mongodb://localhost/comingsoon",
   port:     3000,
   apiUrl:   "http://localhost:3000/api/",
   logLevel: "debug",
--- config/env/production.js
+++ config/env/production.js
@@ -3,8 +3,8 @@
 
 module.exports = {
   db:       process.env.MONGOLAB_URI,
-  port:     80,
-  logLevel: "error",
+  port:     process.env.PORT || '3000',
+  logLevel: process.env.LOG_LEVEL || 'error',
   mailChimp: {
     key: process.env.CHIMP_KEY,
     userList: process.env.CHIMP_USERLIST
